67533ee3d57d968318fa1c3bc48efdb575c098be
nolhan42 <nolhan.gf42@gmail.com>
2025-02-21 09:51:21 +0100

    :hammer: test

---
+++ JS/formulaire.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("my-form").addEventListener("submit", function(event){
+        event.preventDefault();
+
+        let formulaireData = new FormData(this);
+        let formulaireStatus = document.getElementById("my-form-status");
+
+        fetch(this.action, {
+            method: "POST",
+            body: formulaireData,
+            header: {"Accept": "application/json"}
+        })
+        .then(response => {
+            if(response.ok){
+                formulaireStatus.textContent = "Votre message à bien été envoyé";
+                formulaireStatus.style.color = "green";
+                this.reset();
+            }else{
+                formulaireStatus.textContent = "Erreur lors de l'envoi du formulaire.";
+                formulaireStatus.style.color = "red";
+            }
+        })
+        .catch(error =>{
+            formulaireStatus.textContent = "Une erreur à été détecter, veuillez réessayé";
+            formulaireStatus.style.color = "red";
+        });
+    });
+});
